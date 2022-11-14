23e75a1ea98bec080435c6b57521f17ea05d12f7
daongochuy <huydngch190066@fpt.edu.vn>
2022-11-14 10:37:54 +0700

    update day3 homework

---
+++ framework/day1/CoreProject/SelniumAdvanced_Day3/Services/todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelniumAdvanced_day3.Services
+{
+    public partial class Todo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool DoneStatus { get; set; }
+        public string Description { get; set; }
+    }
+}
--- framework/day1/CoreProject/SelniumAdvanced_Day3/SetUp/Day3ProjectSetup.cs
+++ framework/day1/CoreProject/SelniumAdvanced_Day3/SetUp/Day3ProjectSetup.cs
@@ -1,4 +1,5 @@
 ﻿using CoreFramework.UnitTestSetup;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,15 @@ using System.Threading.Tasks;
 namespace SelniumAdvanced_Day3.SetUp
 {
     public class Day3ProjectSetup : NUnitTestSetup
-
     {
+        [SetUp]
+        public void SetUp()
+        {
+            _driver.Url = "https://apichallenges.herokuapp.com";
+        }
+        [TearDown]
+        public void TearDown()
+        {
+        }
     }
 }
